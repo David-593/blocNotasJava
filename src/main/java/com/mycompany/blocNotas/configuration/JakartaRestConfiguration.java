54903a1341e120cdf06742eaba3233d4cb65d005
David-593 <andresdavidm759@gmail.com>
2025-02-17 12:18:49 -0500

    Configuracion del proyecto

---
+++ src/main/java/com/mycompany/blocNotas/configuration/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.blocNotas.configuration;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("/api")
+public class JakartaRestConfiguration extends Application {
+    
+}
