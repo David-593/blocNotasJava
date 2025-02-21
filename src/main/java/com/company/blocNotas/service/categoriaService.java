723fc7c09327564cf845792ecee2c387c2fdffd2
David-593 <andresdavidm759@gmail.com>
2025-02-21 15:33:38 -0500

    Creacion de service para categoria

---
+++ src/main/java/com/company/blocNotas/service/categoriaService.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company.blocNotas.service;
+
+import com.mycompany.blocNotas.entities.Categoria;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+/**
+ *
+ * @author andre
+ */
+public class categoriaService {
+    
+    @PersistenceContext(name = "pruebaPU")
+    private EntityManager em;
+    
+    //Create
+    public void createCategoria (Categoria categoria){
+        em.persist(categoria);
+    }
+    //Read by id
+    public Categoria getCategoriaById(Long cateId){
+        return em.find(Categoria.class, cateId);
+    }
+    
+    //Update
+    public Categoria updateCategoria(Categoria categoria){
+        return em.merge(categoria);
+    }
+    
+    //Delete
+    public void deleteCategoria(Categoria categoria){
+        em.remove(categoria);
+    }
+}
