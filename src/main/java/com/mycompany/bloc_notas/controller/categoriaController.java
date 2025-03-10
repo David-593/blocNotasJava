dbb47fab62cc487cb411942312b5d96fbc2c4a8b
David-593 <andresdavidm759@gmail.com>
2025-03-10 12:22:01 -0500

    Implementacion del nuevo metodo en el controller

--- src/main/java/com/mycompany/bloc_notas/controller/categoriaController.java
+++ src/main/java/com/mycompany/bloc_notas/controller/categoriaController.java
@@ -5,12 +5,14 @@ import com.company.blocNotas.dto.categoriaDto;
 import com.company.blocNotas.service.categoriaService;
 import com.mycompany.blocNotas.entities.Categoria;
 import jakarta.ejb.EJB;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/categorias")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -42,15 +44,28 @@ public class categoriaController {
         }
     }
     
+    //Get all
+    @GET
+    public Response getAllCategories(){
+        try {
+            List<Categoria> categorias = categoriaService.getAllCategories();
+            if (categorias.isEmpty()){
+                return Response.status(Response.Status.NO_CONTENT).build();
+            }
+            return Response.ok(categorias).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
     
     //Update by id
     @PUT
     @Path("/{cateId}")
-    public Response updateCategorie(@PathParam("cateId") Long cateId, Categoria categoria){
+    public Response updateCategorie(@PathParam("cateId") Long cateId, String newCateName){
         try{
-            Categoria categoriaUpdated = categoriaService.updateCategoria(cateId, categoria);
+            Categoria categoriaUpdated = categoriaService.updateCategoria(cateId, newCateName);
             return Response.ok(categoriaUpdated).build();
-        }catch(Exception e){
+        }catch(EntityNotFoundException e){
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
     }
@@ -62,7 +77,7 @@ public class categoriaController {
         try{
             categoriaService.deleteCategoria(cateId);
             return Response.noContent().build();
-        }catch(Exception e){
+        }catch(EntityNotFoundException e){
             return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
         }
     }
