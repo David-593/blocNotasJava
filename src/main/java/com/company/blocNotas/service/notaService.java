287caadef7e56a43e9b88159c2330f7a5f28f255
David-593 <andresdavidm759@gmail.com>
2025-03-12 11:11:11 -0500

    Implementacion del metodo getAllNote en el service

--- src/main/java/com/company/blocNotas/service/notaService.java
+++ src/main/java/com/company/blocNotas/service/notaService.java
@@ -4,17 +4,18 @@
  */
 package com.company.blocNotas.service;
 
+import com.company.blocNotas.dto.NotaDto;
 import com.mycompany.blocNotas.entities.Categoria;
 import com.mycompany.blocNotas.entities.Nota;
 import com.mycompany.blocNotas.entities.Usuario;
+import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import java.util.stream.Collectors;
 
-/**
- *
- * @author andre
- */
+@Stateless
 public class notaService {
     
     @PersistenceContext(name = "pruebaPU")
@@ -23,17 +24,21 @@ public class notaService {
     //Create
     public void CreateNota(Nota nota) throws Exception {
        if (nota.getNotaTitulo() == null || nota.getNotaDescripcion() == null 
-            || nota.getUsuId() == null || nota.getCateId() == null){
-           throw new Exception("Estos campos son obligatorios");
-       }
-       Categoria categoria = em.find(Categoria.class, nota.getCateId());
-        if (categoria == null){
-            throw new Exception("Categoria no encontrada");
+            || nota.getUsuId() == null || nota.getCateId() == null) {
+        throw new Exception("Estos campos son obligatorios");
         }
+    
         Usuario usuario = em.find(Usuario.class, nota.getUsuId());
-        if (usuario == null){
-            throw new Exception("Usuario no encontrado");
+        Categoria categoria = em.find(Categoria.class, nota.getCateId());
+
+        if (usuario == null || categoria == null) {
+            throw new Exception("Usuario o Categoria no encontrados");
         }
+
+        nota.setUsuId(usuario);
+        nota.setCateId(categoria);
+
+        em.persist(nota);
         
        try{
             em.persist(nota); 
@@ -44,27 +49,28 @@ public class notaService {
     }
     
     //Read by id
-    public Nota FindByIdNota(Long notaId)throws EntityNotFoundException{
+    public NotaDto FindByIdNota(Long notaId)throws EntityNotFoundException{
         Nota nota = em.find(Nota.class, notaId);
         if(nota == null){
             throw new EntityNotFoundException("No se ha encontrado esta nota");
         }
-        return nota;
+        return new NotaDto(nota);
     }
     
-    //Delete
-    public void deleteNota(Long notaId) throws Exception{
-        Nota nota = em.find(Nota.class, notaId);
-        if (nota == null){
-            throw new Exception("No se ha encontrado esta nota");
-        }
-        em.remove(nota);
+    //Read all
+    public List<NotaDto> findAllNota(){
+        List<Nota> notas = em.createQuery(
+        "SELECT n FROM Nota n JOIN FETCH n.usuId JOIN FETCH n.cateId", Nota.class)
+        .getResultList();
+    
+    return notas.stream().map(NotaDto::new).collect(Collectors.toList());
     }
+    
     //Update
-    public Nota updateNota(Long notaId, Nota notaActualizada) throws Exception{
+    public Nota updateNota(Long notaId, Nota notaActualizada) throws EntityNotFoundException{
         Nota nota = em.find(Nota.class, notaId);
         if (nota == null){
-            throw new Exception("No se ha encotrado esta nota");
+            throw new EntityNotFoundException("No se ha encotrado esta nota");
         }
         if (notaActualizada.getNotaTitulo() != null){
             nota.setNotaTitulo(notaActualizada.getNotaTitulo());
@@ -75,4 +81,12 @@ public class notaService {
         return em.merge(nota);
     }
    
+    //Delete
+    public void deleteNota(Long notaId) throws EntityNotFoundException{
+        Nota nota = em.find(Nota.class, notaId);
+        if (nota == null){
+            throw new EntityNotFoundException("No se ha encontrado esta nota");
+        }
+        em.remove(nota);
+    }
 }
