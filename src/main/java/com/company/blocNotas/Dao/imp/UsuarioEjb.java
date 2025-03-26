f2472b21ac9e0e07a0d36e1e6157d895a7f8cf03
David-593 <andresdavidm759@gmail.com>
2025-03-26 17:02:06 -0500

    Implementacion de ejb para usuario

--- src/main/java/com/company/blocNotas/Dao/imp/UsuarioEjb.java
+++ src/main/java/com/company/blocNotas/Dao/imp/UsuarioEjb.java
@@ -1,79 +1,64 @@
 
 package com.company.blocNotas.Dao.imp;
 
+import com.company.blocNotas.Dao.itf.IUsuarioEjb;
+import com.mycompany.blocNotas.entities.Usuarios;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
 
-public class UsuarioEjb {
- 
-  /*   @PersistenceContext
+@Stateless(name = "EJB_USUARIOS")
+public class UsuarioEjb implements IUsuarioEjb{
+
+    @PersistenceContext(name = "pruebaPU")
     private EntityManager em;
-    
-    //Usamos DAO este metodo para crear un usuario
-    public void createDAO(JsonObject userJson){
-        UsuarioEntity user = jsonEntity(userJson);
-        em.persist(user);
+    @Override
+    public Usuarios createUsuario(Usuarios usuario) throws Exception {
+        em.persist(usuario);
+        return usuario;
     }
-    
-    //Metodo DAO para buscar un usuario por id y devolver  todo el usuario
-    public UsuarioEntity findUserDAO(Long usuId){
-        return em.find(UsuarioEntity.class, usuId);
+
+    @Override
+    public Usuarios modifyUsuario(Usuarios usuario) throws Exception {
+        if(!existById(usuario.getUsuId())){
+            throw new EntityNotFoundException("Ningun usuario está asociado con ese ID");
+        }
+        return em.merge(usuario);
     }
-    
-    //Metodo DAO para verificar si el usuario existe o no
-    public boolean existUserByIdDAO(Long usuId){
-        Long count = em.createQuery("SELECT COUNT(u) FROM UsuarioEntity u WHERE u.usuId = :id", Long.class)
-                .setParameter("id", usuId)
-                .getSingleResult();
-        return count > 0;
+
+    @Override
+    public Usuarios deleteUsuario(Usuarios usuario) throws Exception {
+        if(!existById(usuario.getUsuId())){
+            throw new EntityNotFoundException("Ningun usuario está asociado con ese ID");
+        }
+        
+        //Se asocia al em para evitar problemas
+        em.merge(usuario);
+        
+        em.remove(usuario);
+        return usuario;
     }
-    
-    //Metodo DAO para obtener todos los usuarios
-    public List<UsuarioEntity> getUsersDAO(){
-        return em.createQuery("SELECT c FROM Usuario c LEFT JOIN FETCH c.notaList", UsuarioEntity.class)
+
+    @Override
+    public Usuarios getUsuarioById(Usuarios usuario) throws Exception {
+        if(!existById(usuario.getUsuId())){
+            throw new EntityNotFoundException("Ningun usuario está asociado con ese ID");
+        } 
+        Usuarios user = em.find(Usuarios.class, usuario.getUsuId());
+        return user;
+    }
+
+    @Override
+    public boolean existById(Integer usuId) throws Exception {
+        return em.find(Usuarios.class, usuId) != null;
+    }
+
+    @Override
+    public List<Usuarios> getAllUsuarios() throws Exception {
+        return em.createQuery("SELECT u FROM Usuarios u LEFT JOIN FETCH c.notaList", Usuarios.class)
                 .getResultList();
     }
-    
-    //Metodo DAO para actualizar un usuario
-    public void updateDAO(Long usuId, JsonObject userJson){
-        if(!existUserByIdDAO(usuId)){
-            throw new EntityNotFoundException("Este usuario no ha sido encontrado para actualizar");
-        }
-        UsuarioEntity userUpdated = jsonEntity(userJson);
-        userUpdated.setUsuId(usuId);
-        em.merge(userUpdated);
-    }
-    
-    //Metodo DAO para eliminar un usuario
-    public void deleteDAO(Long usuId){
-        if(!existUserByIdDAO(usuId)){
-            throw new EntityNotFoundException("Este usuario no ha sido encontrado para eliminar");
-        }
-        UsuarioEntity userDeleted = findUserDAO(usuId);
-        em.remove(userDeleted);
-    }
-    
-    private UsuarioEntity jsonEntity(JsonObject userJson){
-        String name = userJson.getString("name");
-        String lastName = userJson.getString("lastName");
-        
-        //Pasar nacimiento a ObjetoJson transforandolo a String
-        String dateStr = userJson.getString("birthDay");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate formatterDate = LocalDate.parse(dateStr, formatter);
-        
-        String email = userJson.getString("email");
-        Boolean status = userJson.getBoolean("status");
-        
-        //hashear la contraseña con BCrypt luego
-        String password = userJson.getString("password");
-        
-        UsuarioEntity user = new UsuarioEntity();
-        user.setName(name);
-        user.setLastName(lastName);
-        user.setBirthDay(formatterDate);
-        user.setEmail(email);
-        user.setStatus(status);
-        user.setPassword(password);
-        
-        return user;
-    }*/
+ 
 }
--- src/main/java/com/company/blocNotas/Dao/itf/IUsuarioEjb.java
+++ src/main/java/com/company/blocNotas/Dao/itf/IUsuarioEjb.java
@@ -1,9 +1,18 @@
 package com.company.blocNotas.Dao.itf;
+import com.mycompany.blocNotas.entities.Usuarios;
+import java.util.List;
 
-/**
- *
- * @author andre
- */
 public interface IUsuarioEjb {
     
+    Usuarios createUsuario(Usuarios usuario) throws Exception;
+    
+    Usuarios modifyUsuario(Usuarios usuario) throws Exception;
+    
+    Usuarios deleteUsuario(Usuarios usuario) throws Exception;
+    
+    Usuarios getUsuarioById(Usuarios usuario) throws Exception;
+    
+    boolean existById(Integer usuId) throws Exception;
+    
+    List<Usuarios> getAllUsuarios() throws Exception;
 }
