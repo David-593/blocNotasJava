8d809eed1f0764f444cee959c69a45769165feab
David-593 <andresdavidm759@gmail.com>
2025-04-07 17:50:46 -0500

    Eliminacion de lineas innecesarias

--- src/main/java/com/company/blocNotas/service/itf/IUsuarioService.java
+++ src/main/java/com/company/blocNotas/service/itf/IUsuarioService.java
@@ -1,9 +1,19 @@
 package com.company.blocNotas.service.itf;
 
-/**
- *
- * @author andre
- */
+import com.mycompany.blocNotas.entities.Usuarios;
+import jakarta.json.JsonObject;
+import java.util.List;
+
 public interface IUsuarioService {
+    Usuarios createUsuario(JsonObject usuarioJson) throws Exception;
     
+    Usuarios modifyUsuario(JsonObject usuarioJson) throws Exception;
+    
+    Usuarios deleteUsuario(JsonObject usuarioJson) throws Exception;
+    
+    Usuarios getUsuarioByid(JsonObject usuarioJson) throws Exception;
+    
+    boolean existyById(JsonObject usuarioJson) throws Exception;
+    
+    List<Usuarios> getAllUsuarios(JsonObject usuarioJson) throws Exception;
 }
